5cb838980dadbaaa9de5fff9654800318d6df3ef
Corey Noble <corey@slightlysocial.com>
2016-05-05 01:02:39 -0400

    Began work on the Services page

--- src/pages/services.php
+++ src/pages/services.php
@@ -19,30 +19,55 @@
       <h1 class="zoomInDown wow">CMS Skillset<br><hr><small>Corey Noble | 2016</small></h1>
     </div>
 
-    <div class="section black">
-      <div class="row">
-        <div class="large-12 columns">
-          <?php include("../../partials/crumbs.php"); ?>
-          
+    <div class="section white">
+      <div class="row" data-equalizer>
+        <div class= "services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/design-icon.png" alt="Design Icon" />
+          <h4 class="zoomIn wow">Website Design and Branding</h4>
+          <p class="zoomIn wow">I can design the look and feel of your website, create branding, promotional artwork and pretty much anything you need. I have experience with and a thorough understanding of both digital and print media.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/responsive-icon.png" alt="Flexible Layouts Icon" />
+          <h4 class="zoomIn wow">Responsive Development</h4>
+          <p class="zoomIn wow">I build websites that respond to the width of the device you are currenty using and display different layouts depending on the situation. This way your website looks great on anything your visitors may be using.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/seo-icon.png" alt="Search Engine Optimization Icon" />
+          <h4 class="zoomIn wow">SEO and Accessibility</h4>
+          <p class="zoomIn wow">I have training in Search Engine Optimization techniques and Web Content Accessibility Guidelines. This means I can optimize your website to  be accessible to as many people as possible and rank higher in search engine results.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/cms-icon.png" alt="CMS Icon" />
+          <h4 class="zoomIn wow">Content Management Systems</h4>
+          <p class="zoomIn wow">I can build your website with Content Management in mind so you can add/edit pages, media, and even manage your database.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/current-technologies-icon.png" alt="Checkmark Icon" />
+          <h4 class="zoomIn wow">Current Technologies</h4>
+          <p class="zoomIn wow">I always build websites with the latest technologies in mind. Whether it's responsive code, animations, user anylitics, content management plugins or dynamic data, I will keep your website up-to-date with all of the latest trends.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/custom-functionality-icon.png" alt="Puzzle Piece Icon" />
+          <h4 class="zoomIn wow">Custom Functionality</h4>
+          <p class="zoomIn wow">If you need custom functionality on your site, I will most likely be able to accomodate your requests. Please feel free to ask me any questions you may have.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/maintenance-icon.png" alt="Construction Icon" />
+          <h4 class="zoomIn wow">Website Maintenance</h4>
+          <p class="zoomIn wow">I can maintain and update your website on a regular basis, whether you want to change anything, or add something new.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/email-icon.png" alt="Email Icon" />
+          <h4 class="zoomIn wow">Email Campaigns</h4>
+          <p class="zoomIn wow">Do you want to promote campaigns via. email? I have a lot of experience building, testing and deploying email campaigns to large audiences. I can even track click-through anylitics and show you the user data.</p>
+        </div>
+        <div class="services-promotion large-4 medium-4 small-12 columns" data-equalizer-watch>
+          <img class="zoomIn wow" src="assets/img/icons/optimized-code-icon.png" alt="Code Icon" />
+          <h4 class="zoomIn wow">Clean and Efficient Code</h4>
+          <p class="zoomIn wow">I always optimize the the code I write to ensure minimal load times. My code is written in an understandabe, programatic format so it is easy for any web developer to read and manipulate.</p>
         </div>
       </div>
     </div>
-
-    <div class="section blue">
-      <div class="row">
-        <div class="large-12 columns">
-
-        </div>
-      </div>
-  </div>
-
-  <div class="section white">
-    <div class="row">
-      <div class="large-12 columns">
-
-      </div>
-    </div>
-  </div>
 
   <div id="parallax-cms-skillset" class="parallax hide-tablet hide-mobile" data-stellar-background-ratio="0.8" ></div>
 
