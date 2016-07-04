65df1d5d1841ce1a35d5de79645d7d285b8d420d
Corey Noble <coreynoble@hotmail.ca>
2016-07-03 22:18:23 -0400

    Created Blog page and structure with a test entry

---
+++ src/pages/blog.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Blog | Corey Noble</title>
+    <link rel="icon" type="image/jpeg" href="http://www.corey-noble.com/favicon.jpg" />
+
+    <!-- Relative CSS / Javascript -->
+    <link rel="stylesheet" href="http://www.corey-noble.com/assets/css/app.css" />
+    <link rel="stylesheet" href="http://www.corey-noble.com/assets/css/animate.css" />
+    <script src="http://www.corey-noble.com/bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <!-- Navigation Menu -->
+    <?php include("http://www.corey-noble.com/partials/navigation.html"); ?>
+
+
+
+
+
+<!-- BLOG HEADER -->
+  <div class="section blue">
+    <div class="row">
+      <div class="large-12 columns">
+        <h1 class="header-no-parallax slideInLeft wow">Welcome to My Blog</h1>
+        <h4 class="slideInLeft wow">The purpose of this blog is to create a space to share the <b>ideas</b> and discuss the <b>topics</b> that are on my mind.</h4>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+<!-- BLOG ENTRIES -->
+  <div class="section white">
+    <div class="row">
+      <div class="large-12 columns">
+        <h2 class="fadeInDown wow" >Entries</h2>
+        <hr class="fadeInDown wow">
+      </div>
+
+
+
+
+
+      <!-- BLOG ENTRY 1 -->
+      <div class="blog-entry fadeInUp wow large-12 columns">
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
+        </a>
+      </div>
+
+      <!-- BLOG ENTRY 2 -->
+      <div class="blog-entry fadeInUp wow large-12 columns">
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
+        </a>
+      </div>
+
+      <!-- BLOG ENTRY 3 -->
+      <div class="blog-entry fadeInUp wow large-12 columns">
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
+        </a>
+      </div>
+
+      <!-- BLOG ENTRY 4 -->
+      <div class="blog-entry fadeInUp wow large-12 columns">
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
+        </a>
+      </div>
+
+      <!-- BLOG ENTRY 5 -->
+      <div class="blog-entry fadeInUp wow large-12 columns">
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+    <!-- Footer -->
+    <?php include("http://www.corey-noble.com/partials/footer.html"); ?>
+
+    <!-- Scripts -->
+    <?php include("http://www.corey-noble.com/partials/scripts.html"); ?>
+  </body>
+</html>
