90770f849c12f75ffed28c7f46741a58b6ceca6a
Corey Noble <coreynoble@hotmail.ca>
2016-07-05 22:52:22 -0400

    not sure why these are being removed

--- src/pages/blog.php
+++ src/pages/blog.php
@@ -24,8 +24,8 @@
   <div class="section blue">
     <div class="row">
       <div class="large-12 columns">
-        <h1 class="header-no-parallax fadeInDown wow">Welcome to My Blog</h1>
-        <h4 class="fadeInDown wow">The purpose of this blog is to create a space to share the <b>ideas</b> and discuss the <b>topics</b> that are on my mind.</h4>
+        <h1 class="header-no-parallax slideInLeft wow">Welcome to My Blog</h1>
+        <h4 class="slideInLeft wow">The purpose of this blog is to create a space to share the <b>ideas</b> and discuss the <b>topics</b> that are on my mind.</h4>
       </div>
     </div>
   </div>
@@ -46,95 +46,49 @@
 
 
 
-      <!-- Project Repositories  -->
+      <!-- BLOG ENTRY 1 -->
       <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/project-repositories.php">
-          <h3>Project Repositories</h3>
-          <p></p>
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
         </a>
       </div>
 
-      <!-- Web Technologies I Use  -->
+      <!-- BLOG ENTRY 2 -->
       <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/web-technologies-i-use.php">
-          <h3>Web Technologies I Use</h3>
-          <p></p>
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
         </a>
       </div>
 
-      <!-- Teams or Departments?  -->
+      <!-- BLOG ENTRY 3 -->
       <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/terms-or-departments.php">
-          <h3>Teams or Departments?</h3>
-          <p></p>
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
         </a>
       </div>
 
-      <!-- Website Development Workflows -->
+      <!-- BLOG ENTRY 4 -->
       <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/agile-methodoligy.php">
-          <h3>Website Development Workflows (Agile Methodoligy)</h3>
-          <p></p>
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
         </a>
       </div>
 
-      <!-- WCAG -->
+      <!-- BLOG ENTRY 5 -->
       <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/web-content-accessibility-guidelines.php">
-          <h3>Web Content Accessibility Guidelines</h3>
-          <p></p>
-        </a>
-      </div>
-
-      <!-- Search Engine Optimization -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/search-engine-optimization-techniques.php">
-          <h3>Search Engine Optimization Techniques</h3>
-          <p></p>
-        </a>
-      </div>
-
-      <!-- My Ideal Workplace -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/ideal-workplace.php">
-          <h3>My Ideal Workplace</h3>
-          <p></p>
-        </a>
-      </div>
-
-      <!-- My Career Aspirations -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/career-aspirations.php">
-          <h3>My Career Aspirations</h3>
-          <p>The direction I would like to head as a Web Developer.</p>
-        </a>
-      </div>
-
-      <!-- Wedding Invitations -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/wedding-invitations.php">
-          <h3>I Made Custom Invitations for my Wedding</h3>
-          <p>My process for creating custom Invitations and Save the Date's for both web and print.</p>
-        </a>
-      </div>
-
-      <!-- Web Production Specialist -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/web-production-specialist.php">
-          <h3>Web Production Specialist</h3>
-          <p>What is a Web Production Specialist? My role is at Thrillworks Inc.</p>
-        </a>
-      </div>
-
-      <!-- Blog Introduction -->
-      <div class="blog-entry fadeInUp wow large-12 columns">
-        <a href="/blog/blog-introduction.php">
-          <h3>Blog Introduction</h3>
-          <p>The idea, reason and rules of my blog.</p>
+        <a href="/blog/blog-introduction-3-7-16.php">
+          <h3>Test Blog</h3>
+          <p>This is the short description explaining breifly the blog entry topic and contents within.</p>
         </a>
       </div>
     </div>
   </div>
+
+
 
 
 
