01a62cc32530d11c630760719eb73e6d6e107703
Corey Noble <coreynoble@hotmail.ca>
2016-12-13 23:57:32 -0500

    Finishing Web Production Specialist Blog Entry

--- src/pages/blog/web-production-specialist.php
+++ src/pages/blog/web-production-specialist.php
@@ -23,7 +23,7 @@
   <div class="section blue">
     <div class="row">
       <div class="large-12 columns">
-        <h1 class="header-no-parallax fadeInDown wow">My Role as a Web Production Specialist</h1>
+        <h1 class="header-no-parallax fadeInDown wow">My Role as a Web Production Specialist at Thrillworks</h1>
       </div>
     </div>
   </div>
@@ -37,24 +37,35 @@
       <div class="row">
         <div class="large-12 columns">
           <h3 class="fadeInDown wow">What is Web Production Specialist?</h3>
-          <p class="fadeInDown wow">In short, a Web Production Specialist at Thrillworks is a 'jack of all trades' role with focus on content authoring, email campaigns, web production graphics, and front-end web development.</p>
+          <p class="fadeInDown wow">In short, a Web Production Specialist at Thrillworks is a 'Jack of all trades' role with focus on: Content authoring, email campaigns, web production graphics, and front-end web development.</p>
           <!-- Finish Intro -->
-          <p class="fadeInDown wow">Below are examples of various tasks that may be required of my position, on a day-to-day basis.</p>
+          <p class="fadeInDown wow">Below are examples of various tasks and methods that are required of my position on a day-to-day basis.</p>
           
           <hr class="fadeInDown wow">
           
+          <!-- Web Production Graphics -->
+          <h3 class="fadeInDown wow">Web Production Graphics</h3>
+          <p class="fadeInDown wow">Using photo-editing software, primarilly Adobe Photoshop; I manipulate designs and output images to work with the needs of a project. This can include:</p>
+          <ul>
+              <li>Slicing and saving out optimized images in various file types and dimensions.</li>
+              <li>Updating fonts, copy, colour, UIs and graphics.</li>
+              <li>Translating content and modifying products.</li>
+          </ul>
+          <p class="fadeInDown wow">I am typically provided finished designs and it is my job to ensure all graphics are ready for production. If there is re-work required on a design or asset, it is typically my job to make adjustments.</p>
+            
+          <hr class="fadeInDown wow">
+            
           <!-- Email Campaigns -->
           <h3 class="fadeInDown wow">Email Campaigns</h3>
           <p class="fadeInDown wow">Using technologies like Exact Target and Customer.io, I build, test and deploy .html and .txt email campaigns.</p>
           <p class="fadeInDown wow">Typical Process:</p>
           <ol>
-            <li class="fadeInDown wow">I am provided .psd designs of approved emails. I slice out the images that I need and save them out as web graphics with specific naming conventions.</li>
-            <li class="fadeInDown wow">I upload all of the sliced images to the email client server and note their final URLs and dimensions so I can rapidly add images to the .html later on.</li>
+            <li class="fadeInDown wow">I am provided .PSDs of approved emails. I slice out the images that I need and save them out as web graphics with specific naming conventions.</li>
+            <li class="fadeInDown wow">I upload all of the sliced images to the email client and note their final URLs / dimensions so I can rapidly add images to the .html later on.</li>
             <li class="fadeInDown wow">I copy a pre-existing email with the structure that is closest to what I need. Typically the header and footer stay the same as a pre-existing email so copying is more expedient to the process.</li>
             <li class="fadeInDown wow">I gut the copied emails (.html and .txt) and keep only the structure that will be required for the build.</li>
             <li class="fadeInDown wow">I build out the emails by referencing any spreadsheets, copydecks and designs that have been provided. This is when I add all of the images, anylitics, alt text, links, and copy.</li>
-            <li class="fadeInDown wow">When done, I send test emails to internal staff for review.</li>
-            <li class="fadeInDown wow">After the review process, typically a few items will need to change. I make any nessesary updates and re-send test emails.</li>
+            <li class="fadeInDown wow">When done, I upload the .html and .txt files to the email client and send test emails to myself and internal leadership for review. Then there is a feedback loop, first internally, then with the client.</li>
             <li class="fadeInDown wow">When all emails are ready to be sent there can be some additional criteria to be met. That can involve:
                 <ul>
                     <li>Setting up a scheduled deployment in batches (eg. 15,000/hr) to account for server load on popular campaigns.</li>
@@ -65,77 +76,75 @@
           </ol>
             
           <hr class="fadeInDown wow">
-          
-          <!-- Web Production Graphics -->
-          <h3 class="fadeInDown wow">Web Production Graphics</h3>
-          <p class="fadeInDown wow">Using Photoshop, I manipulate designs and images to work best with the needs of given web projects. This can include but is not limited to; optimizing file sizes, modifying dimensions, changing fonts, text and colours.</p>
-            
-          <hr class="fadeInDown wow">
             
           <!-- Content Authoring -->
           <h3 class="fadeInDown wow">Content Authoring</h3>
-          <p class="fadeInDown wow">Using technologies like WordPress, RedDot, Magento, SiteCore and Adobe Experience Manager I manage websites.</p>
-          <p class="fadeInDown wow">This can include:</p>
+          <p class="fadeInDown wow">As a Web Production Specialist I author web pages, edit content and hook up assets using Content Management Systems. The technologies I use most often are Adobe&nbsp;Experience&nbsp;Manager, Open Text's RedDot, WordPress, Magento, SiteCore and Joomla.</p>
+          <p class="fadeInDown wow">The tasks I perform can include:</p>
           <ul>
-            <li class="fadeInDown wow">Creating new web pages from provided copydecks, spreadsheets and/or designs.</li>
+            <li class="fadeInDown wow">Creating and building new web pages based on provided copydecks, spreadsheets and/or designs.</li>
             <li class="fadeInDown wow">Modifying existing web pages.</li>
             <li class="fadeInDown wow">Creating and managing multi-lingual versions of web pages.</li>
-            <li class="fadeInDown wow">Managing digital assets (images, videos, documents)</li>
-            <li class="fadeInDown wow">Managing products, promotional banners and press releases</li>
-            <li class="fadeInDown wow">Workflowing pages and creating backup data</li>
-            <li class="fadeInDown wow">Setting up Anylitics</li>
+            <li class="fadeInDown wow">Managing and hooking up digital assets (images, videos, documents).</li>
+            <li class="fadeInDown wow">Managing products, promotional banners and press releases.</li>
+            <li class="fadeInDown wow">Workflowing pages from Author to Staging, and from Staging to Production.</li>
+            <li class="fadeInDown wow">Versioning pages, Backing up files and managing page data.</li>
+            <li class="fadeInDown wow">Setting up anylitics, meta-data and page re-directs.</li>
           </ul>
             
           <hr class="fadeInDown wow">
             
           <!-- Front-End Web Development -->
           <h3 class="fadeInDown wow">Front-End Web Development</h3>
-          <p class="fadeInDown wow">Front-End Web Deveopment involves creating websites and web components with HTML, CSS, Javascript and PHP code. When I am asked to work on a Front-End project, typically it involves:</p>
+          <p class="fadeInDown wow">Front-End Web Deveopment involves creating or modifying websites with HTML, SCSS, Javascript and PHP code. When I work on a Front-End project, typically it includes many of the tasks in 'Content Authoring', but can also include:</p>
           <ul>
-              <li class="fadeInDown wow">Pulling a code repository onto my computer and using source control to manage the project.</li>
-              <li class="fadeInDown wow">Using a front-end framework and SCSS to develop/modify web pages and components.</li>
-              <li class="fadeInDown wow">Providing testing URLs when completed.</li>
-              <li class="fadeInDown wow">Providing packages for production or publishing live when approved.</li>
+              <li class="fadeInDown wow">Creating/Pulling a code repository onto my computer, and using source control to manage the project.</li>
+              <li class="fadeInDown wow">Using a front-end framework and HTML/SCSS/JS/PHP to develop and modify web projects.</li>
+              <li class="fadeInDown wow">Collaborating with other developers, ensuring code standards are met and that there are no conflicts in the repository.</li>
+              <li class="fadeInDown wow">Staging project work on a testing environment and providing testing URLs.</li>
+              <li class="fadeInDown wow">Creating packages for production or publishing approved work.</li>
           </ul>
             
           <hr class="fadeInDown wow">
             
           <!-- Ticket Tracking -->
           <h3 class="fadeInDown wow">Ticket Tracking</h3>
-          <p class="fadeInDown wow">Once a deliverable has been sent to QA and the project team, any issues that have been spotted are turned into tickets that are tracked using JIRA and a project number. We use this ticket process to track the number of issues and their importance. We mark the status of the issues and talk about them on a granular level. This gives everyone on the project oversight.</p>
+          <p class="fadeInDown wow">Once a deliverable has been sent for review, any issues that have been spotted are turned into tasks using software like JIRA. We use this ticket process to ensure all issues are tracked, marked for their importance, and eventually resolved. This gives everyone oversight on the project, its status and the amount of work left to&nbsp;do.</p>
             
           <hr class="fadeInDown wow">
           
           <!-- Training Documentation -->
           <h3 class="fadeInDown wow">Training Documentation</h3>
-          <p class="fadeInDown wow">If a new process is developed, or a client/internal staff require training on a process, we create detailed training documentation. This includes a step-by-step document with both visual and textual representation of the process. We try to include as many relvant resources and examples as possible, and when we solve a task we  either edjucate as to why we use a particular process, or explain alternative methods.</p>
-          <p class="fadeInDown wow">Internal documentation at Thrillworks is created as a Wiki, using Confluence. Documents to clients are provided in the format that they are requested in, typically designed PDFs.</p>
+          <p class="fadeInDown wow">If a new process is developed, or if a client/internal staff require training on a process, we create detailed training documentation. This includes a step-by-step document with both a visual and textual representation of the process. In the document we include as many relvant resources and examples as possible.</p>
+          <p class="fadeInDown wow">Internal documentation at Thrillworks is created as a Wiki. Documents to clients are provided in the format that they are requested in, typically designed PDFs.</p>
            
-          <hr class="fadeInDown wow">
-            
-          <!-- Special Projects -->
-          <h3 class="fadeInDown wow">Special Projects</h3>
-          <p class="fadeInDown wow">If there is any free time at Thrillworks, I am able to pursue special projects. This can include learning new technologies, developing new business skillsets or building tools that help ease the burden of a task. In the 2 years I have worked at Thrillworks I have been able to:</p>
-          <ul>
-            <li class="fadeInDown wow">Create an internal website to help identify the various banners on a client website, Staging and Production. It allows for a quick comparison between the two, and helps speed up the QA of Owned Assets.</li>
-            <li class="fadeInDown wow">Create a new process for developing responsive email campaigns. Using Foundation for Emails, I created and fully tested a email templates that accounted for every use case Thrillworks might need when it comes to responsive email development. This was a major upgrade to their original process and allowed Thrillworks to expand on the services they offer as a company.</li>
-            <li class="fadeInDown wow">Developed a Web Content Accessibility Guidelines initiative that involved creating learning material, testing material and labs.</li>
-           </ul>
-            
           <hr class="fadeInDown wow">
             
           <!-- Agile Methodoligy -->
           <h3 class="fadeInDown wow">Agile Methodoligy</h3>
-          <p class="fadeInDown wow">Thrillworks used to have a waterfall approach to web development. This means that a project would start with one department, and make its way through the departments until a project was completed. This was problematic as projects could get stuck and timelines would be tight as a result, creating stress and affecting the bottom line. Their solution to this problem was to completely revamp the way work was done at Thrillworks. They brought in Agile specialists and the entire company learned Agile Methodoligy.</p>
-          <p class="fadeInDown wow">Agile Methodoligy includes:</p>
+          <p class="fadeInDown wow">Thrillworks <b>used</b> to take a waterfall approach to project work. A waterfall process involves starting a project with one department (UX) and moving through the other departments (Design/Copy > Development/Production > QA) one after another, until a project is complete. This was problematic as the departments further down the chain would often see their timelines cut due to discrepencies with departments up the chain. This created stress and affected profit margins.</p>
+          <p class="fadeInDown wow">Their solution to this problem was to completely revamp the way work was done at Thrillworks. The company brought in specialists on Agile development and the everyone learned Agile Methodoligy.</p>
+          <p class="fadeInDown wow">Agile Methodoligy is a very different approach to project work, opposed to the typical waterfall approach. It allows Thrillworks to increase profit and accellerate timelines.</p>
+          <p class="fadeInDown wow">A breif synopsis of Agile:</p>
           <ul>
-            <li class="fadeInDown wow">No more departments. Teams are created to handle specific clients and include the skillsets that cover the needs of the client.</li>
-            <li class="fadeInDown wow">A new workflow process in-which a project is broken down into pieces of work, as granularly as possible. All of the pieces of work are put up on a board in 'swim-lanes' and marked as either backlog, in-progress, blocked, staging, completed. Due to this approach, work can start almost immediately, with enough oversight for everyone to be able to work individually without affecting another.</li>
+            <li class="fadeInDown wow">No more departments. Teams are created to handle specific clients. These teams include the skillsets that cover the needs of the client.</li>
+            <li class="fadeInDown wow">A new workflow process in-which a project is broken down into pieces of work, as granularly as possible and marked with time estimates. All of the pieces of work are put up on a board and categoriezed by their stauts: backlog, in progress, blocked, ready for review or completed. Tasks are taken by a team member with the skills to work on it, they 'own' it for the time they are working on it. Due to this approach, some work that would normally start later in the process can start almost immediately. Nobody is ever working on the same work at the same time, this avoids project collision.</li>
             <li class="fadeInDown wow">Daily team meetings occur to bring up concerns and keep the status of projects in-check.</li>
             <li class="fadeInDown wow">Retrospectives meetings are scheduled to help iron out issues that came up throughout a project. This helps improve upon processes for the next time.</li>
-            <li class="fadeInDown wow">Employees are T-shaped, meaning that they are encouraged to help out when they can, even if it is a different workflow or technology that they are not used to. This helps ensure there are no knowledge silos and teams work well together.</li>
+            <li class="fadeInDown wow">Employees are T-shaped, meaning that they are encouraged to help out when they can, even if it is a different workflow or technology that they are not used to. This helps ensure there are no knowledge silos and that teams share the workload.</li>
           </ul>
             
+          <hr class="fadeInDown wow">
+            
+          <!-- Special Projects -->
+          <h3 class="fadeInDown wow">Special Projects</h3>
+          <p class="fadeInDown wow">Sometimes there are opportunities at Thrillworks to pursue special projects. These opportunities can come from an internal initiative to better the company, or, if time permits, the opportunity for self improvement. This can include learning new technologies, developing new business skills or building tools that help ease the burden of a task. In the 2.5 years I have worked at Thrillworks I have been able to:</p>
+          <ul>
+            <li class="fadeInDown wow">Create an internal quality assurance tool that displays all of the promotional banners on a client website. The tool shows the banners on their staging and production environments. It allows for a quick comparison between the two environments, and helps speed up the testing process.</li>
+            <li class="fadeInDown wow">Create a new process for developing responsive email campaigns. Using Foundation for Emails, I created and fully tested email templates that accounted for every use case Thrillworks might need when it comes to responsive email development. This was a major upgrade to their original process and allowed Thrillworks to expand on the services they offer as a company.</li>
+            <li class="fadeInDown wow">Develop a learning initiative on the Web Content Accessibility Guidelines (WCAG). This involved creating learning material based on the WCAG guidelines; Creating testing material and working labs. This initiative ensures all staff are aware of the legal accessibility requirements that all websites need to meet.</li>
+            <li class="fadeInDown wow">Collaborate with a team of 7 people to introduce and standardize the company-wide use of a digital Kanban board software.</li>
+           </ul>
         </div>
       </div>
     </div>
