bacd0d2688330d049c1c097150f7521c2b56247b
Corey Noble <coreynoble@hotmail.ca>
2019-03-31 23:51:52 -0400

    Back to Top - Adding + Styling Button

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,17 @@ import Foundation from 'foundation-sites';
 //import './lib/foundation-explicit-pieces';
 
 $(document).foundation();
+
+// Back to Top
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 1200) {       // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(800);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(800);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 1000);
+});
