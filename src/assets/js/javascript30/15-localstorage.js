125d1fe340be27952492d75ee9fbd8098a615181
Corey Noble <coreynoble@hotmail.ca>
2019-06-02 21:32:02 -0400

    JavaScript30 - 15 - Resolving syntax by comparing with final

--- src/assets/js/javascript30/15-localstorage.js
+++ src/assets/js/javascript30/15-localstorage.js
@@ -2,26 +2,27 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-function addItem(e){
+function addItem(e) {
     e.preventDefault(); // stops the page from reloading
-    const text = (this.querySelector('[name=item')).value;
+    const text = (this.querySelector('[name=item]')).value;
     const item = {
         text,
         done: false
-    }
+    };
+
     items.push(item);
     populateList(items, itemsList);
     localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 }
 
-function populateList(plates = [], platesList){
+function populateList(plates = [], platesList) {
     platesList.innerHTML = plates.map((plate, i) => {
         return `
-            <li>
-                <input type="checkbox" data-index${i} id="item${i}" ${plate.done ? 'checked' : ''} />
-                <label for="item${i}">${plate.text}</label>
-            </li>
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+            <label for="item${i}">${plate.text}</label>
+        </li>
         `;
     }).join('');
 }
