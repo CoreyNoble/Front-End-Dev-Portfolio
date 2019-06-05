d51c3d86443b598e00b331da03eadf7fd488dc11
Corey Noble <coreynoble@hotmail.ca>
2019-06-04 23:41:30 -0400

    JavaScript30 - 21 - Resolving syntax

--- src/assets/js/javascript30/21-geolocation.js
+++ src/assets/js/javascript30/21-geolocation.js
@@ -1,10 +1,11 @@
 const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('speed-value');
+const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
+    console.log(data);
     speed.textContent = data.coords.speed;
-    arrow.getElementsByClassName.transform = `rotate(${data.coords.heading}deg)`;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 }, (err) => {
-    console.err(err);
-    alert('The funtionality of this application requires access to your geo-location');
+    console.error(err);
+    alert('This application requires access to your geo-location in-order to function');
 });
