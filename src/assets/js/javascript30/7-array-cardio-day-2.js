3b1f888d0462108edaa732b01743b3bc7ebe803a
Corey Noble <coreynoble@hotmail.ca>
2019-06-11 21:08:18 -0400

    JavaScript30 - 7 - Comments

--- src/assets/js/javascript30/7-array-cardio-day-2.js
+++ src/assets/js/javascript30/7-array-cardio-day-2.js
@@ -15,23 +15,23 @@ const comments = [
 ];
 
 // Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-// Array.prototype.every() // is everyone 19 or older?
+// Array.prototype.some() // some: At least one.
+// Array.prototype.every() // every: All of them.
 
-// ONE Adult
+// One adult is >= 19
 const oneAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
 
 console.log('Is there an adult in this group?');
 console.log(oneAdult);
 
-// ALL Adults
+// All adults are >= 19
 const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
 
 console.log('Are all people in this group adults?');
 console.log(allAdults);
 
 // Array.prototype.find()
-// Find is like filter, but instead returns just the one you are looking for
+// Find is like filter, but instead of returning a subset of the array, it is going to return the first one that it finds.
 // find the comment with the ID of 823423
 const comment = comments.find(comment => comment.id === 823423);
 
@@ -39,14 +39,19 @@ console.log('Find a specific comment in an array:');
 console.table(comment);
 
 // Array.prototype.findIndex()
-// Find the comment with this ID
-// delete the comment with the ID of 823423
+// Find the index of the comment with this ID
 const index = comments.findIndex(comment => comment.id === 823423);
 
-// comments.splice(index, 1);
+// Now delete the comment with the ID of 823423
 
+// comments.splice(index, 1); //splice: removes the item in the array.
+
+// new array of updated comments.
 const newComments = [
+    // ... spread the items into the new array
+    // Start at 0, go until index
     ...comments.slice(0, index),
+    // start at index + 1
     ...comments.slice(index + 1)
 ];
 
