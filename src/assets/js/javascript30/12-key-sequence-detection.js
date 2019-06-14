05c2ee5125b28b777a7dd5c283a9386a896b4baa
Corey Noble <coreynoble@hotmail.ca>
2019-06-13 20:10:43 -0400

    JavaScript30 - 12 - Comments

--- src/assets/js/javascript30/12-key-sequence-detection.js
+++ src/assets/js/javascript30/12-key-sequence-detection.js
@@ -1,10 +1,16 @@
+// Initialize empty array
 const pressed = [];
+// Code we're looking to detect
 const secretCode = '3838404037393739666513';
 
+// When key up, pass the event
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
+  // Push the key press into the array.
   pressed.push(e.key);
+  // Trim the array to ensure we're only looking for the maximum number of characters.
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  // If when we join the array and it matches the secretCode, do something.
   if (pressed.join('').includes(secretCode)) {
     console.log('DING DING!');
     cornify_add();
