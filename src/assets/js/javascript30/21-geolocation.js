6ba64ef01c8c052e58c074f90d9ab973d897936f
Corey Noble <coreynoble@hotmail.ca>
2019-06-18 00:19:41 -0400

    JavaScript30 - 21 + 22 - Comments

--- src/assets/js/javascript30/21-geolocation.js
+++ src/assets/js/javascript30/21-geolocation.js
@@ -1,11 +1,15 @@
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
+const arrow = document.querySelector('.arrow'); // Compass
+const speed = document.querySelector('.speed-value'); // Speed
 
+// Listen for the users' updated position. 'watchPosition'.
+// 'getPosition' is static and is only called once.
 navigator.geolocation.watchPosition((data) => {
     console.log(data);
+    // Set the text content of 'speed' to the value of 'data.coords.speed' (Default is km/h).
     speed.textContent = data.coords.speed;
+    // Rotate the Compass using the value from 'data.coords.heading' as the set degree of rotation (# of degrees relative to north).
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-}, (err) => {
+}, (err) => { // User denys the webcam request.
     console.error(err);
     alert('This application requires access to your geo-location in-order to function');
 });
--- src/assets/js/javascript30/22-follow-along-link-highlighter.js
+++ src/assets/js/javascript30/22-follow-along-link-highlighter.js
@@ -1,22 +1,35 @@
+// Page. Parent <div>.
 const page = document.querySelector("#javascript30-22");
+// Grab every <a> on 'page'.
 const triggers = page.querySelectorAll('a');
+// <span> element that follows along.
 const highlight = document.createElement('span');
+// Add a class of 'highlight' to the <span>
 highlight.classList.add('highlight');
+// Append <span> as a child element to 'page'
 page.appendChild(highlight);
 
+// Moves the <span> to the position of the hovered <a> inside of 'page.
 function highlightLink() {
+    // The bounding client rectangle of the <a> (this).
     const linkCoords = this.getBoundingClientRect();
     console.log(linkCoords);
+    // Coordinates object (Width, Height, Left, Top) populated from the 'linkCoords' bounding rectangle.
     const coords = {
+        // Width / Height of the rectangle.
         width: linkCoords.width,
         height: linkCoords.height,
+        // Position of the <a> from the (top/left) of the window, PLUS the scroll value (scrollY/scrollX).
         top: linkCoords.top + window.scrollY,
         left: linkCoords.left + window.scrollX
     };
 
+    // Set the Width / Height of the <span> to match the Width / Height of the <a> rectangle.
     highlight.style.width = `${coords.width}px`;
     highlight.style.height = `${coords.height}px`;
+    // Set the Position (scrollX, scrollY) of the <span> to match the position of the <a> rectangle.
     highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
+// On each <a>, when mouse enters the element, highlightLink().
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
